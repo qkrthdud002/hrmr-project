9cbb00d129773cf22ec9839e74eeba52f08d1ea3
sejun7 <moon060927@naver.com>
2021-10-12 20:35:10 +0900

    todolist

--- hrmr/routes/todolist.js
+++ hrmr/routes/todolist.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('todolist', { title: 'Express' });
+  res.render('todolist', { title: 'todo' });
 });
 
 module.exports = router;

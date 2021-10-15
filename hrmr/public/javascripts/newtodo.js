0766728c7b2d04fd0725d6efec96c16f6eae244e
이윤성 <80931771+leeyunseongKing@users.noreply.github.com>
2021-10-15 13:23:34 +0900

    newtodo

--- hrmr/public/javascripts/newtodo.js
+++ hrmr/public/javascripts/newtodo.js
@@ -1,1 +1,16 @@
-function editItem()
+const toDoForm = document.querySelector(".toDoForm");
+const toDoInput = document.querySelector(".toDoForm input");
+
+const onSubmit = (event) =>{
+    event.preventDefault();
+    const currentInputValue = toDoInput.value;
+    console.log(currentInputValue);
+    toDoInput.value = ""    
+}
+
+const init = () =>{
+    console.log(toDoInput.value)
+    toDoForm.addEventListener("submit", onSubmit);
+}
+
+init()

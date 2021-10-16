19cb8c3b3fae34fae8d004c62325e0768459fea0
qkrthdud002 <qkrthdud002@gmail.com>
2021-10-16 15:02:34 +0900

    mainpage

--- hrmr/routes/todolist.js
+++ hrmr/routes/todolist.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('todolist', { title: 'todo' });
+  res.render('todolist', { title: 'Express' });
 
-  res.render('todolist', { title: 'Todo' });
+  //res.render('todolist', { title: 'Todo' });
 
+  //res.render('newtodo', {title: 'newtodo'})
 });
 
 module.exports = router;

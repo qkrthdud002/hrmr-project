0d6ffdfb6805e102420b47ba9e163632ba2d8ad9
sejun7 <moon060927@naver.com>
2021-10-19 23:26:26 +0900

    Update todolist.js

--- hrmr/routes/todolist.js
+++ hrmr/routes/todolist.js
@@ -5,11 +5,15 @@ var db = require('../model/dbcp');
 /* GET home page. */
 router.get('/', async (req, res)=> {
   const query_date = req.query.date;
+  let today = new Date();
   if(query_date==undefined){
     //query_date에 오늘 날짜 저장.
+    query_date = today;
   }
 
   //DB에서 주어진 날짜의 todo list조회
+  
+
 
   // DB에서 주어진 날짜의 time_reoced 조회
 

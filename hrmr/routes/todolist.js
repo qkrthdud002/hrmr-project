8022218e8feeca90b55023443617f769228330b3
qkrthdud002 <qkrthdud002@gmail.com>
2021-10-19 18:31:51 +0900

    todo.js & todo.pug

--- hrmr/routes/todolist.js
+++ hrmr/routes/todolist.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const todolist=[
+    {todoId:1, title:'todo1'},
+    {todoId:2, title:'todo2'},
+    {todoId:3, title:'todo3'},
+    {todoId:4, title:'todo4'},
+  ];
 
-  res.render('todolist', { title: 'Express' });
+  res.render('todolist', { todolist: todolist });
 
   //res.render('todolist', { title: 'Todo' });
 

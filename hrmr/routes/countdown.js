2fe1272bba5c83a467eeaf5ea810497d948b46a8
hyeonmi3 <cc146@daum.net>
2021-10-20 23:44:22 +0900

    oh...no..HLEPMEEEE

--- hrmr/routes/countdown.js
+++ hrmr/routes/countdown.js
@@ -4,6 +4,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('countdown', { title: 'Express' });
+
+  $(document).ready(function() {
+    var timer;
+    //시작
+    $('#countdown_button_start').click(function(){
+        timer = setInterval(function(){
+        var timer_sec  = Number($("#span_sec").text());
+        $("#span_sec").text(timer_sec+1);
+        },1000);
+    });
+  
+    //중지
+    $('#countdown_button_pause').click(function(){
+      clearInterval(timer);
+    });
+  });
 });
 
 module.exports = router;

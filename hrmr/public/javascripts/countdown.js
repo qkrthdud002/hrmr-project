e62a3349407c88e97bc66b2e69a0282ad1c83a7e
Kinggod <80931771+leeyunseongKing@users.noreply.github.com>
2021-10-26 20:24:21 +0900

    countdown

--- hrmr/public/javascripts/countdown.js
+++ hrmr/public/javascripts/countdown.js
@@ -1,9 +1,14 @@
 $(()=>{
     $('#start').on('click', ()=>{
-        const hour = $('#target_hour').val();;
-        const min = $('#target_min').val();;
-        const sec = $('#target_sec').val();;
+        const hour = $('#target_hour').val();
+        const min = $('#target_min').val();
+        const sec = $('#target_sec').val();
+        if(hour == '' && min == '' && sec == ''){
+            alert('시간을 입력하세요.');
+            return;
+        }
         
         $('#time').text(hour + ':' + min + ':' + sec);
     });
+ 
 });

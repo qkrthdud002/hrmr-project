f1161904f62a839787e8db628714e41b00a2a7db
bomin-317 <82502927+bomin-317@users.noreply.github.com>
2021-10-26 19:37:26 +0900

    stopwatch

--- hrmr/routes/stopwatch.js
+++ hrmr/routes/stopwatch.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { off, response } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:todoId', async (req, res) => {
+router.get(['/','/:todoId'], async (req, res) => {
   const todoId=req.params.todoId;
+  if(todoId==null || todoId==undefined){
+    res.render('stopwatch');
+    return;
+  }
 
   // db에서 주어진 todoId를 이용해 todo정보 조회
   const rows = await conn.query('select * from todo where todo_id=?',[todoId])
@@ -18,10 +23,13 @@ router.get('/:todoId', async (req, res) => {
 
 router.post('/:todoId', async (req, res)=>{
   const todoId=req.params.todoId;
+  const {start_time, end_time} = req.body;
 
   // time_record 테이블에 시간 정보 저장
+  const Data = `${todo_id}, ${start_time}, ${end_time}`
+  db.query(`INSERT INTO time_record(todo_id, start_time, end_time) VALUES(${Data})`)
   
-  res.redirect('/todolist');
+  res.json({result:'ok'});
 });
 
 module.exports = router;

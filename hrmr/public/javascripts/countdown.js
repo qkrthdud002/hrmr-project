e3307587e3388a23c0caf1611ef499d5b9b0fc99
Kinggod <80931771+leeyunseongKing@users.noreply.github.com>
2021-10-28 23:49:55 +0900

    countdown

--- hrmr/public/javascripts/countdown.js
+++ hrmr/public/javascripts/countdown.js
@@ -18,7 +18,9 @@ $(()=>{
         }
         let timernumber = window.setInterval(()=>{
             console.log(1)
-            sec = sec - 1;
+            if(sec != 0){
+                sec = sec - 1;
+            }
             if(min == 0 && hour != 0){
                 hour--;
                 min = 60;
@@ -27,7 +29,7 @@ $(()=>{
                 min--;
                 sec = 60;
             }
-            else if(sec == 0 && min == 0 && hour == 0){
+            else if(sec == -1 && min == 0 && hour == 0){
                 // $('#time').text(String(hour).padStart(2,'0') + ":" + String(min).padStart(2,'0') + ":" + String(sec).padStart(2,'0'));
                 alert('끝.');
                 window.clearInterval(timernumber)
